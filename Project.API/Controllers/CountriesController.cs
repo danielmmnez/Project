9b027cb0cd6e79def9170cd8e13139d72ad9b279
danielmmnez <danielmm0202@gmail.com>
2023-05-05 18:06:36 -0500

    Creation GetCombo of CountriesController

--- Project.API/Controllers/CountriesController.cs
+++ Project.API/Controllers/CountriesController.cs
@@ -141,5 +141,12 @@ namespace Project.API.Controllers
 
             return NoContent();
         }
+
+        [AllowAnonymous]
+        [HttpGet("combo")]
+        public async Task<ActionResult> GetCombo()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
     }
 }
